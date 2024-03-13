2db96c43ce42eaa0cc2f32190276cff55cb704f4
john habaguhirwa <johnhabaguhirwa@student-10-201-05-005.wireless.adelaide.edu.au>
2024-03-13 14:55:52 +1030

    ghjh

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -1,13 +1,5 @@
 #include "workshop.h"
 
-double *dynamicArray(int N)
-{
-    double *darr = new double[N];
-    for (int i = 0; i < 10; i++)
-        darr[i] = i;
-    return (darr);
-}
-
 double *dynamicArray(int N, double M)
 {
     double *darr = new double[N];
